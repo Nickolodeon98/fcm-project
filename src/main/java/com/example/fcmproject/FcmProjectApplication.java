894b9c17df18cf1b4ec135a41fe7f67e2e4dd535
Nickolodeon98 <sjeon0730@gmail.com>
2023-07-21 15:57:06 +0900

    feat: create a new project

---
+++ src/main/java/com/example/fcmproject/FcmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.fcmproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcmProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FcmProjectApplication.class, args);
+  }
+
+}
